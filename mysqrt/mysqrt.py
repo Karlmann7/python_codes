743701e00d7967c3f509b890270ed291c4ded657
Karlmann7 <carlos.plua.g@gmail.com>
2018-05-27 15:10:41 +0200

    convert mysqrt.py to module and function

--- mysqrt/mysqrt.py
+++ mysqrt/mysqrt.py
@@ -1,13 +1,13 @@
 
-x = 0.01
-s = 1.
-kmax = 100
-tol  = 1.e-14
-for k in range(kmax):
-	print("Before iteration ", k, ", s = ", s)
-	s0 = s
-	s = 0.5*(s + x/s)
-	delta_s = s - s0
-	if abs(delta_s/x) < tol:
-		break
-print("After ", k+1, " iteration, s = ", s)
+def sqrt2(x):
+	s = 1.
+	kmax = 100
+	tol  = 1.e-14
+	for k in range(kmax):
+		print("Before iteration %s, s = %20.15f" %(k, s))
+		s0 = s
+		s = 0.5*(s + x/s)
+		delta_s = s - s0
+		if abs(delta_s/x) < tol:
+			break
+	print("After %s iteration, s = %20.15f" %(k+1,s))
