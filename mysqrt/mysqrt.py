c363e2724517a1f20a78020529243fe180b48589
Karlmann7 <carlos.plua.g@gmail.com>
2018-05-27 14:51:50 +0200

    add convergence test

--- mysqrt/mysqrt.py
+++ mysqrt/mysqrt.py
@@ -1,7 +1,13 @@
 
-x = 4.
+x = 0.01
 s = 1.
-for k in range(6):
+kmax = 100
+tol  = 1.e-14
+for k in range(kmax):
 	print("Before iteration ", k, ", s = ", s)
+	s0 = s
 	s = 0.5*(s + x/s)
+	delta_s = s - s0
+	if abs(delta_s/x) < tol:
+		break
 print("After ", k+1, " iteration, s = ", s)
